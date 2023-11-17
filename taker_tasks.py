b04c17b47c2d9774d924963b32d6cbb378e58a5c
vldsq <147376341+vldsq@users.noreply.github.com>
2023-11-17 21:37:23 +0500

    Update taker_tasks.py

--- taker_tasks.py
+++ taker_tasks.py
@@ -61,7 +61,7 @@ def handle_callback_query(call):
             markup.add(*buttons)
             bot.send_message(chat_id, text='Выберите задачу, которую хотите изменить', reply_markup=markup)
 
-        if data == 'all_tasks':
+    if data == 'all_tasks':
         text = 'Все задачи:\n'
         for i, task_id in enumerate(all_tasks.keys(), start=1):
             task_info = all_tasks[task_id]
